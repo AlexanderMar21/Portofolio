b34dd520e412ebe5b026791565ed838577de9a7a
AlexanderMar21 <54232340+AlexanderMar21@users.noreply.github.com>
2021-08-27 09:16:59 +0200

    update: nav bg in mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 // Display the Current Year on website
 const publishYear = "2020"; // year of website creation
-const currentYear = document.getElementById("current-year"); 
+const currentYear = document.getElementById("current-year");
 const year = new Date().getFullYear();
 // year-stamp
 if ( publishYear == year)
     currentYear.innerText = year ;
-else 
+else
     currentYear.innerText =publishYear + " - " + year ;
 
 const toggleButton = document.getElementById("hamBurger");
@@ -15,7 +15,7 @@ const personalStatementView = document.querySelector("#auto-typing");
 const topMenuButton = document.querySelector("#top-arrow");
 
 
-// Array of typed words 
+// Array of typed words
 const personalStatements = ["Full Stack Developer" , "Hobbyist Photographer" , "Designer" , "Thirsty Traveller"];
 
 
@@ -24,23 +24,29 @@ const personalStatements = ["Full Stack Developer" , "Hobbyist Photographer" , "
 const navabar = document.querySelector("#navbar");
 let scrolled = false;
 
-// change nav-bar from transparent to visible background on first scroll
-window.onscroll = function(){
-    if (window.pageYOffset > 90){
-        navabar.classList.remove("transparent");
-    }else {
-        navabar.classList.add("transparent");
-    }
-}
 // Resize window
-
 var client_w = document.documentElement.clientWidth;
+var client_h = document.documentElement.clientHeight;
+if ( client_w < 550 && client_h < 700 ){
+    navabar.classList.remove("transparent");
+}else {
+    // change nav-bar from transparent to visible background on first scroll
+    window.onscroll = function(){
+        if (window.pageYOffset > 90){
+            navabar.classList.remove("transparent");
+        }else {
+            navabar.classList.add("transparent");
+        }
+    }
+
+}
+
 // Move button on  window resize
 window.onresize = () => {
     client_w = document.documentElement.clientWidth;
     if ( client_w < 940 ) {
         topMenuButton.classList.add("top-arrow-mobile")
-    }else 
+    }else
         topMenuButton.classList.remove("top-arrow-mobile")
 }
 
@@ -55,25 +61,25 @@ $(window).scroll(function () {
     }
     // hightlight links on scroll
 
-        $('.nav-link').each(function (event) {
-            if  ( widnowTopOffset >= $($(this).attr('href')).offset().top - 170) {
-                $('.nav-link').parent().not(this).removeClass('active');
-                $(this).parent().addClass('active');
-            }else if (widnowTopOffset < $($(this).attr('href')).offset().top - 190) { 
-                $(this).parent().removeClass('active');
-            }
-        });
-   
+    $('.nav-link').each(function (event) {
+        if  ( widnowTopOffset >= $($(this).attr('href')).offset().top - 170) {
+            $('.nav-link').parent().not(this).removeClass('active');
+            $(this).parent().addClass('active');
+        }else if (widnowTopOffset < $($(this).attr('href')).offset().top - 190) {
+            $(this).parent().removeClass('active');
+        }
+    });
+
 });
 
-// auto typing text one 
+// auto typing text one
 setTimeout(() => {
     var typed = new Typed("#auto-typing" , {
-        strings: personalStatements , 
+        strings: personalStatements ,
         typeSpeed : 140 ,
-        cursorChar: '_', 
-        backSpeed : 80 , 
-        loop : true 
+        cursorChar: '_',
+        backSpeed : 80 ,
+        loop : true
     });
 }, 3800);
 
@@ -89,7 +95,7 @@ toggleButton.addEventListener('click' ,  ()=> {
 
 // outside menu
 $(document).on("click", function(e){
-    if( 
+    if(
       $(e.target).closest("#nav-ul").length == 0 &&
       $("#nav-ul").hasClass("show") &&
       $(e.target).closest("#hamBurger").length == 0
